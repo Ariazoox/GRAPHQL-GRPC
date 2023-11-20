095d1872df9fe73cc00ed311d9d48527afc22a8f
Ariazoox <101866115+Ariazoox@users.noreply.github.com>
2023-11-20 17:32:54 +0100

    Update user.py

--- user/user.py
+++ user/user.py
@@ -55,3 +55,63 @@ def get_user_bookings(userid):
 
         returned_booking["userid"] = booking.userid
         returned_booking["dates"] = []
+       
+ for date_item in booking.dates:
+        mapped_movies = []
+        for movie_id in date_item.movies:
+            # Requête GRAPHQL qui remplace la requête REST
+            query = """
+                {
+                    movie_with_id (_id: "%s"){
+                        title,
+                        id,
+                        rating,
+                        director
+                    }
+                }
+            """ % (
+                movie_id
+            )
+            # Appel du service movie
+            res = requests.post(url="http://localhost:3001/graphql", json={"query": query})
+            movie = res.json()
+            mapped_movies.append(movie["data"]["movie_with_id"])
+
+        returned_booking["dates"].append(
+            {"date": date_item.date, "movies": mapped_movies}
+        )
+
+    return make_response(jsonify(returned_booking), 200)
+
+# Ajoute un booking pour un utilisateur selon son ID donné en paramètre
+@app.route("/users/<userid>/bookings", methods=["POST"])
+def book_for_user(userid):
+    data = request.get_json()
+    
+    # Appel du service booking 
+    with grpc.insecure_channel("localhost:3002") as channel:
+        
+        stub = booking_pb2_grpc.BookingStub(channel)
+        # Appel de la fonction AddBookingByUser du service booking
+        try:
+            stub.AddBookingByUser(
+                booking_pb2.BookingPayload(
+                    userid=userid, date=data["date"], movieid=data["movieid"]
+                )
+            )
+            return make_response(jsonify({"message": "Booking added successfully"}))
+        # Message d'erreur si la vérification échoue
+        except Exception as e:
+            print(e)
+            return make_response(
+                jsonify(
+                    {
+                        "error": "The date or the movieid is not correct, make sure the date is in the showtime table, that the movie is part of it, and that it's not already reserved."
+                    }
+                ),
+                400,
+            )
+
+if __name__ == "__main__":
+   print("Server running on port %s"%(PORT))
+   app.run(host=HOST, port=PORT)
