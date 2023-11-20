ce646d2541983e4d458f423f7e52954dc328b361
Ariazoox <101866115+Ariazoox@users.noreply.github.com>
2023-11-20 17:40:47 +0100

    Update movie.py

--- movie/movie.py
+++ movie/movie.py
@@ -8,9 +8,10 @@ PORT = 3001
 HOST = '0.0.0.0'
 app = Flask(__name__)
 
-
-
+# Chargement du schéma GraphQL depuis un fichier externe
 type_defs = load_schema_from_path('movie.graphql')
+
+# Création des objets GraphQL
 query = QueryType()
 movie = ObjectType('Movie')
 query.set_field('movie_with_id', r.movie_with_id)
@@ -18,24 +19,23 @@ mutation = MutationType()
 mutation.set_field('update_movie_rate', r.update_movie_rate)
 actor = ObjectType('Actor')
 movie.set_field('actors', r.resolve_actors_in_movie)
+
+# Création du schéma exécutable
 schema = make_executable_schema(type_defs, movie, query, mutation, actor)
 
-
-# point d'entrée qui renvoie à la page d'accueil "Welcome to the Movie service!"
+# Point d'entrée renvoyant à la page d'accueil "Welcome to the Movie service!"
 @app.route("/", methods=['GET'])
 def home():
-    return make_response("<h1 style='color:blue'>Welcome to the Movie service!</h1>",200)
+    return make_response("<h1 style='color:blue'>Welcome to the Movie service!</h1>", 200)
 
-
-# renvoie vers le Playground graphql.
+# Redirige vers le Playground GraphQL.
 @app.route('/graphql', methods=['GET'])
 def playground():
     return PLAYGROUND_HTML, 200
 
-# Permet de tester des requête Graphql 
+# Permet de tester des requêtes GraphQL
 @app.route('/graphql', methods=['POST'])
 def graphql_server():
-
     data = request.get_json()
     success, result = graphql_sync(
         schema,
@@ -47,5 +47,5 @@ def graphql_server():
     return jsonify(result), status_code
 
 if __name__ == "__main__":
-    print("Server running in port %s"%(PORT))
+    print("Server running on port %s"%(PORT))
     app.run(host=HOST, port=PORT)
