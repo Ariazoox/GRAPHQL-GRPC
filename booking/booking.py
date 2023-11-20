b6a31bb7a79cea9861405f32db4c5c244d79bab3
Ariazoox <101866115+Ariazoox@users.noreply.github.com>
2023-11-20 17:43:06 +0100

    Update booking.py

--- booking/booking.py
+++ booking/booking.py
@@ -13,33 +13,33 @@ class BookingServicer(booking_pb2_grpc.BookingServicer):
         with open('{}/data/bookings.json'.format("."), "r") as jsf:
             self.db = json.load(jsf)["bookings"]
 
-    # Fonction qui récupère les résérvations d'un utilisateur 
+    # Fonction qui récupère les réservations d'un utilisateur 
     def GetUserBookings(self, request, context):
         # Boucle qui parcourt nos bookings
         for booking in self.db:
-            # Vérification de l'existence de l'ID dans le booking.json
+            # Vérification de l'existence de l'ID dans le fichier bookings.json
             if request.userid == booking["userid"]:
                 return booking_pb2.BookingData(
                     userid=booking["userid"], dates=booking["dates"])
     
-    # Fonction qui ajoute une résérvation pour un utilisateur en faisant appel à Showtime pour la vérification
+    # Fonction qui ajoute une réservation pour un utilisateur en faisant appel à Showtime pour la vérification
     def AddBookingByUser(self, request, context):
         schedule = []
         with grpc.insecure_channel("localhost:3000") as channel:
             stub = showtime_pb2_grpc.ShowtimeStub(channel)
             schedule = stub.GetShwotime(showtime_pb2.Empty()).schedule
-        # Boucle qui parcourt nos schedule
+        # Boucle qui parcourt nos schedules
         for schedule_item in schedule:
-            # On vérifie la correspodance des dates/movieid
+            # On vérifie la correspondance des dates/movieid
             if (
                     schedule_item.date == request.date
                     and request.movieid in schedule_item.movies
             ):
-                # Boucle qui parcourt les données de booking.json
+                # Boucle qui parcourt les données de bookings.json
                 for booking in self.db:
-                    # Vérification de l'id
+                    # Vérification de l'ID
                     if booking["userid"] == request.userid:
-                        # Boucle pour parcourir les dates dans booking.json
+                        # Boucle pour parcourir les dates dans bookings.json
                         for item in booking["dates"]:
                             # Vérification des dates
                             if item["date"] == request.date:
@@ -75,7 +75,7 @@ def serve():
     booking_pb2_grpc.add_BookingServicer_to_server(BookingServicer(), server)
     server.add_insecure_port('[::]:3002')
     server.start()
-    print("Starting")
+    print("Server started")
     server.wait_for_termination()
 
 
