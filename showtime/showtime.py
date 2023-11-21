2e2308827bc4741971e30c08b499e52ba282658f
Ariazoox <101866115+Ariazoox@users.noreply.github.com>
2023-11-21 14:47:24 +0100

    Update showtime.py

--- showtime/showtime.py
+++ showtime/showtime.py
@@ -15,15 +15,6 @@ class ShowtimeServicer(showtime_pb2_grpc.ShowtimeServicer):
 
         return showtime_pb2.ScheduleData(date='', movies=[])
 
-    # Fonction qui renvoie les films affichés à une date donnée
-    def Get_movie_by_date(self, request, context):
-        # Boucle qui parcourt nos schedules dans times.json
-        for booking in self.db:
-            # Vérification de la correspondance de la date
-            if booking['date'] == request.date:
-                return showtime_pb2.ScheduleData(date=booking['date'], movies=booking['movies'])
-
-        return showtime_pb2.ScheduleData(date='', movies=[])
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
